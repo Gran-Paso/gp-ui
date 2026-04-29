37afa275f47ad90fa241d3cdd3fc2c18728f3613
Prieto <pablojavierprietocepeda@gmail.com>
2026-04-28 23:35:32 -0400

    patch para poder conectar

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
@@ -18,6 +21,7 @@ export default defineConfig({
         'theme/tailwind-preset': resolve(__dirname, 'src/theme/tailwind-preset.ts'),
       },
       formats: ['es', 'cjs'],
+      cssFileName: 'style',
     },
     rollupOptions: {
       external: [
